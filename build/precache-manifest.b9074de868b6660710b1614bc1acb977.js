57254ebfda70da894852b1135e973be9274d8ecb
DESKTOP-DG3BGOU\Pierrot <pmorin@gmail.com>
2019-04-05 16:17:41 -0400

    send build folder

---
+++ build/precache-manifest.b9074de868b6660710b1614bc1acb977.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e8238f864538c66f032",
+    "url": "/static/css/4.b5e5edc9.chunk.css"
+  },
+  {
+    "revision": "b5a358e79abc4e5bf09f",
+    "url": "/static/css/main.23048e86.chunk.css"
+  },
+  {
+    "revision": "0fdf40a5f726874440e3",
+    "url": "/static/js/runtime~main.0fdf40a5.js"
+  },
+  {
+    "revision": "fc6b7dca262933e2e983",
+    "url": "/static/js/2.fc6b7dca.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "b0a4e905f3fdda73dbb2",
+    "url": "/static/js/3.b0a4e905.chunk.js"
+  },
+  {
+    "revision": "b5a358e79abc4e5bf09f",
+    "url": "/static/js/main.b5a358e7.chunk.js"
+  },
+  {
+    "revision": "7e8238f864538c66f032",
+    "url": "/static/js/4.7e8238f8.chunk.js"
+  },
+  {
+    "revision": "5713497b686f28dfea58",
+    "url": "/static/js/5.5713497b.chunk.js"
+  },
+  {
+    "revision": "5713497b686f28dfea58",
+    "url": "/static/css/5.befacc58.chunk.css"
+  },
+  {
+    "revision": "b0a4e905f3fdda73dbb2",
+    "url": "/static/css/3.517308fb.chunk.css"
+  },
+  {
+    "revision": "f914de47d89b36c99421554ad4e4cc06",
+    "url": "/index.html"
+  }
+];
